d678c9afc1d3d8d6dbbf51b794f5fcefa3103028
Pavlo Kreitsberg <pavel.kreitsberg@gmail.com>
2023-04-06 08:30:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/motor-sich/">
+    <BrowserRouter basename="motor-sich">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
